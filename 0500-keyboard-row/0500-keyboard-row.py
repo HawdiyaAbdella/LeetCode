71176eeb2e06af45cbdb10df115e12c30d655750
Hawdiya Abdella <hawdiyaaomer@gmail.com>
2025-08-15 21:41:47 +0300

    Time: 0 ms (100%), Space: 17.5 MB (96.52%) - LeetHub

---
+++ 0500-keyboard-row/0500-keyboard-row.py
@@ -0,0 +1,21 @@
+class Solution:
+    def findWords(self, words: List[str]) -> List[str]:
+      
+        keyboard = {
+            1: set("qwertyuiop"),
+            2: set("asdfghjkl"),
+            3: set("zxcvbnm")
+        }
+        
+        result = []
+        for word in words:
+            word_lower = word.lower() 
+            row = None 
+            for key, value in keyboard.items():
+                if word_lower[0] in value: 
+                    row = key
+                    break
+            if row is not None and all(char in keyboard[row] for char in word_lower): 
+                result.append(word)
+        
+        return result
