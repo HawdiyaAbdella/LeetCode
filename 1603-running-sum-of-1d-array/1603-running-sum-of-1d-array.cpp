74055ea7d96be432a760ad46e3eb6009670ef4e4
Hawdiya Abdella <hawdiyaaomer@gmail.com>
2025-10-31 23:25:33 +0300

    Time: 1 ms (9.32%), Space: 12.5 MB (49.18%) - LeetHub

---
+++ 1603-running-sum-of-1d-array/1603-running-sum-of-1d-array.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> runningSum(vector<int>& nums) {
+        int sum=0;
+        vector<int>result;
+     for (int i=0; i<=nums.size()-1;i++) {
+         sum+=nums[i];
+        result.push_back(sum);
+     }
+     return result;
+     
+
+    }
+};
